f146c287d844d8c9f56c2343c215c6fbdf204c41
Tyro Elohim <kayrakyzaghan11turkishgods.com@gmail.com>
2024-04-04 13:13:08 +0530

    second commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import Note from "../components/Note";
+import "../styles/Home.css";
+
+function Home() {
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((response) => response.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((response) => {
+        if (response.status === 204) alert("Note deleted successfully");
+        else alert("Failed to delete...");
+        getNotes();
+      })
+      .catch((error) => alert(error));
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((response) => {
+        if (response.status === 201) alert("Note Created!!");
+        else alert("Failed to make note!");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Notes</h2>
+        {notes.map((note) => (
+          <Note note={note} onDelete={deleteNote} key={note.id} />
+        ))}
+      </div>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title: </label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="content">Content:</label>
+        <br />
+        <textarea
+          id="content"
+          name="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The Page you're looking for does'nt exist!!</p>
+    </div>
+  );
+}
+
+export default NotFound;
